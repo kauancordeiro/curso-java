89b62a32bd192ae7fb50afc84aa37ced2a633544
kauancordeiro <cvitorcordeiro@gmail.com>
2023-03-07 23:46:12 -0300

    refazendo primeiro exercicio de interface

---
+++ interface_ex01revisando/src/model/entities/Invoice.java
@@ -0,0 +1,45 @@
+package model.entities;
+
+public class Invoice {
+	
+	private Double basicPayment;
+	private double tax;
+	
+	
+	public Invoice() {
+		
+	}
+
+
+	public Invoice(Double basicPayment, double tax) {
+		
+		this.basicPayment = basicPayment;
+		this.tax = tax;
+	}
+
+
+	public Double getBasicPayment() {
+		return basicPayment;
+	}
+
+
+	public double getTax() {
+		return tax;
+	}
+
+
+	public void setBasicPayment(Double basicPayment) {
+		this.basicPayment = basicPayment;
+	}
+
+
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+
+	public Double getTotalPayment() {
+		return getBasicPayment() - getTax();
+	}
+	
+
+}
